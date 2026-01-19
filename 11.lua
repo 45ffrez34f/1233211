654105928554c81e1dd65d8ee57733084327de98
45ffrez34f <stualet555@gmail.com>
2026-01-20 00:03:38 +0700

    Create 11.lua

---
+++ 11.lua
@@ -0,0 +1,191 @@
+local authData = {
+    users = {
+        ["1063496238017818705"] = {
+            keys = {
+                ["win dusel"] = {
+                    hwids = {
+                        "",
+                    },
+                    executors = {
+                        "Solara",
+                    },
+                    expiresAt = "lifetime", -- DD.MM.YY или "lifetime"
+                }
+            },
+            banned = false
+        },
+        ["1326271318546452568"] = {
+            keys = {
+                ["KxYmJrTgLpVfHbNzDsCwAeRhQuIoPkLaSdFgHjJkZlXcVbNmQqW2"] = {
+                    hwids = {
+                        "",
+                    },
+                    executors = {
+                        "",
+                    },
+                    expiresAt = "lifetime",
+                }
+            },
+            banned = false
+        },
+        ["1009482637846524005"] = {
+            keys = {
+                ["bZxCcVbBnNmMkKjJhGnRGfFdDsSaApPoOiIuUyYtTrReWwQqLlK"] = {
+                    hwids = {
+                        "2ff1855e-e5a1-11f0-9fdf-806e6f6e6963",
+                    },
+                    executors = {
+                        "Xeno",
+                    },
+                    expiresAt = "lifetime",
+                }
+            },
+            banned = false
+        },
+        ["USER_ID_4"] = {
+            keys = {
+                ["aFgHjKlMnBvCxZqWeRtYuIoPlMjNhBgVfCcXxZzQqWwE"] = {
+                    hwids = {
+                        "",
+                    },
+                    executors = {
+                        "",
+                    },
+                    expiresAt = "",
+                }
+            },
+            banned = false
+        },
+        ["USER_ID_5"] = {
+            keys = {
+                ["rTyYuIoOpPlLkKjJhHgFgFdDsSaAzzXxCcVvBbNnMmW"] = {
+                    hwids = {
+                        "",
+                    },
+                    executors = {
+                        "",
+                    },
+                    expiresAt = "",
+                }
+            },
+            banned = false
+        },
+        ["USER_ID_6"] = {
+            keys = {
+                ["eWwQqAaSsDdFfGgHhJjKkLlZzXxCcVvBbNnMmOoIiU"] = {
+                    hwids = {
+                        "",
+                    },
+                    executors = {
+                        "",
+                    },
+                    expiresAt = "",
+                }
+            },
+            banned = false
+        },
+        ["USER_ID_7"] = {
+            keys = {
+                ["uUyYtTrReEwWqQzZxCvVbBnNmMlKlKjJhHgFgFdDsS"] = {
+                    hwids = {
+                        "",
+                    },
+                    executors = {
+                        "",
+                    },
+                    expiresAt = "",
+                }
+            },
+            banned = false
+        },
+        ["USER_ID_8"] = {
+            keys = {
+                ["pPoOiIuUyYtTrReEwWqQzZxCcVvBbNnMmLlKkJhHgF"] = {
+                    hwids = {
+                        "",
+                    },
+                    executors = {
+                        "",
+                    },
+                    expiresAt = "",
+                }
+            },
+            banned = false
+        },
+        ["USER_ID_9"] = {
+            keys = {
+                ["kKlLjJhHgFgFdDsSaAqQwWeErRtTyYuUiIoOpPzZxCc"] = {
+                    hwids = {
+                        "",
+                    },
+                    executors = {
+                        "",
+                    },
+                    expiresAt = "",
+                }
+            },
+            banned = false
+        },
+        ["USER_ID_10"] = {
+            keys = {
+                ["mMnNbBvVCcCxXxZzZlLiIoOuUyYtTrReEwWqQaSsD"] = {
+                    hwids = {
+                        "",
+                    },
+                    executors = {
+                        "",
+                    },
+                    expiresAt = "",
+                }
+            },
+            banned = false
+        },
+        ["USER_ID_11"] = {
+            keys = {
+                ["dDsSaAqQwWeErRtTyYuUiIoOpPlLkKjJhHgFgZzXxC"] = {
+                    hwids = {
+                        "",
+                    },
+                    executors = {
+                        "",
+                    },
+                    expiresAt = "",
+                }
+            },
+            banned = false
+        },
+        ["USER_ID_12"] = {
+            keys = {
+                ["vVbBnNmMkKjJhHgFgFdDsSaApPoOiIuUyYtTrReWwQ"] = {
+                    hwids = {
+                        "",
+                    },
+                    executors = {
+                        "",
+                    },
+                    expiresAt = "",
+                }
+            },
+            banned = false
+        },
+        ["USER_ID_13"] = {
+            keys = {
+                ["zZxXcCvVbBnNmMlKlKjJhHgFgFdDsSaAqQwWeErRtT"] = {
+                    hwids = {
+                        "",
+                    },
+                    executors = {
+                        "",
+                    },
+                    expiresAt = "",
+                }
+            },
+            banned = false
+        }
+    },
+    bannedHwids = {
+        "",
+    }
+}
+
+return authData
